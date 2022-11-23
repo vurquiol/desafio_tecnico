4340b570b1809b0f965d6974f476e7342ba349ae
Victor Urquiola <victoru160@gmail.com~>
2022-11-22 23:40:36 -0300

    test

--- database/config.js
+++ database/config.js
@@ -5,10 +5,10 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
    const dbConnection = async() => {
     
     
-    const uri = "mongodb+srv://vurquiol:RH4JDWXZej527g2y@cluster0.2f3cz6z.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://vurquiol:RH4JDWXZej527g2y@cluster0.2f3cz6z.mongodb.net/desafio_tecnico";
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     client.connect(err => {
-    const collection = client.db("desafio_tecnico").collection("usuarios");
+    //const collection = client.db("desafio_tecnico").collection("usuarios");
     // perform actions on the collection object
     client.close();
     });
