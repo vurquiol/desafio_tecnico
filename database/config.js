65277d9695489b21ca8e07ce6ad696b82cac5d0c
Victor Urquiola <victoru160@gmail.com~>
2022-11-23 09:23:01 -0300

    conf

--- database/config.js
+++ database/config.js
@@ -5,19 +5,14 @@ require('dotenv').config();
 const dbConnection = async() => {
     
     try{
-        const conexion = await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-          });
-         console.log('MongoDB connected...', conexion.connection.host);
+        await mongoose.connect(process.env.DB_CNN);
+        console.log('MongoDB Conected...');
     }catch (error){
         console.log(error);
     }
     
 
-};
+}
 
 
-module.exports = dbConnection;
+module.exports = {dbConnection};
